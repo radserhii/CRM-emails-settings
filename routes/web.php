a715f905e8964a764f992a3c705d073250ce1d9b
Serj Jacobs <serj.j@callfin.ltd>
2018-09-05 15:58:53 +0300

    remove old files

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,23 @@ Route::name('crm.')->group(function () {
     Route::get('/', 'CRM\IndexController@dashboard');
     Route::get('/traders', 'CRM\IndexController@traders')->name('traders');
     Route::get('/trader/{id}', 'CRM\TradersController@getTrader')->name('getTrader');
-    Route::group(['prefix'=> 'data'], function() {
+    Route::group(['prefix' => 'data'], function () {
         Route::get('getCalls', 'ImportController@getCalls')->name('calls');
         Route::post('getTraders', 'CRM\TradersController@getTraders')->name('getTraders');
     });
-
-//    Email page
-    Route::get('/emails', 'CRM\IndexController@emails')->name('emails');
-//    Email settings
-    Route::get('/emails/settings', 'CRM\EmailSettingController@getAllSettings');
-    Route::post('/emails/settings', 'CRM\EmailSettingController@updateSettings');
-//    Email actions
-    Route::get('/emails/actions', 'CRM\EmailActionController@getAllActions');
-    Route::post('/emails/actions', 'CRM\EmailActionController@updateActions');
+    Route::group(['prefix' => 'emails'], function () {
+        //    Email page
+        Route::get('/', 'CRM\IndexController@emails')->name('emails');
+        //    Email settings
+        Route::get('settings', 'CRM\EmailSettingController@getAllSettings');
+        Route::post('settings', 'CRM\EmailSettingController@updateSettings');
+        //    Email actions
+        Route::get('actions', 'CRM\EmailActionController@getAllActions');
+        Route::post('actions', 'CRM\EmailActionController@updateActions');
+        //    Email templates
+        Route::get('templates', 'CRM\EmailTemplateController@getAllTemplates');
+        Route::post('templates', 'CRM\EmailTemplateController@updateTemplate');
+    });
 });
 
 
